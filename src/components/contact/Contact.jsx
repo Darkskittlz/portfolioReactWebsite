1f97ba7e65a18723bbb9045307f23758b400f323
Darkskittlz <darkskiiittles@gmail.com>
2021-10-11 09:22:39 -0400

    responsive email form - emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,34 @@
 import "./contact.scss";
 import { useState } from 'react';
 import { GitHub, LinkedIn } from "@material-ui/icons";
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
 
 export default function Contact() {
     const [message,setMessage] = useState(false)
 
-    const handleSubmit=(e) => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
         e.preventDefault();
         setMessage(true)
+
+    emailjs.sendForm(
+        'service_wigaog9', 
+        'template_f7p5sjq',
+        form.current,
+        'user_5ir65fREMKq7eQSSWuTYq',
+        )
+        .then((result) => {
+            console.log('SUCCESS!', result.status, result.text);
+         }, (err) => {
+            console.log('FAILED...', err.text);
+         });
+        e.target.reset();
     }
+    
+    
     return (
         <div className="contact" id="contact">
         <div className="left">
@@ -28,10 +48,11 @@ export default function Contact() {
                 </div>
             </div>
             <h2>Contact</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Email" />
-                <textarea placeholder="Message"></textarea>
-                <button type="submit">Send</button>
+            <form ref={form} onSubmit={sendEmail}>
+                <input type="text" name="user_name" placeholder="NAME" style = {{margin: "5px", padding: "5px"}}/>
+                <input type="email" name="user_email" placeholder="EMAIL" style = {{padding: "5px"}}/>
+                <textarea name="message" placeholder="MESSAGE" style = {{padding: "5px"}}/>
+                <input id="send" type="submit" value="Send" />
                 {message && <span>Thanks, I'll reply ASAP :)</span>}
             </form>
         </div>
